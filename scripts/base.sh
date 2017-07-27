9d800c9d522595d175ad1cd6995a00d40b2c2d7b
seanly <seanly@aliyun.com>
2017-07-27 10:38:37 +0800

    delete timezone configure in script

--- scripts/base.sh
+++ scripts/base.sh
@@ -4,6 +4,5 @@ yum -y install gcc make gcc-c++ kernel-devel-`uname -r` perl wget bzip2
 mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
 wget http://mirrors.163.com/.help/CentOS${RELEASE}-Base-163.repo -O /etc/yum.repos.d/CentOS-Base.repo
 yum -y update
-timedatectl  set-timezone Asia/Shanghai
 reboot
 sleep 60
