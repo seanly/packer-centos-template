73173ea3576e9df4d890d4fb72edd75657214fbf
seanly <seanly@aliyun.com>
2017-07-27 10:20:27 +0800

    adjust use json configure

--- scripts/base.sh
+++ scripts/base.sh
@@ -2,7 +2,7 @@ RELEASE=`rpm -q --qf "%{VERSION}" $(rpm -q --whatprovides redhat-release)`
 rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-${RELEASE}.noarch.rpm
 yum -y install gcc make gcc-c++ kernel-devel-`uname -r` perl wget bzip2
 mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup
-wget http://mirrors.163.com/.help/CentOS7-Base-163.repo -O /etc/yum.repos.d/CentOS-Base.repo
+wget http://mirrors.163.com/.help/CentOS${RELEASE}-Base-163.repo -O /etc/yum.repos.d/CentOS-Base.repo
 yum -y update
 timedatectl  set-timezone Asia/Shanghai
 reboot
--- scripts/puppet.sh
+++
@@ -1,3 +0,0 @@
-RELEASE=`rpm -q --qf "%{VERSION}" $(rpm -q --whatprovides redhat-release)`
-rpm -Uvh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-${RELEASE}.noarch.rpm
-yum install -y puppet-agent
